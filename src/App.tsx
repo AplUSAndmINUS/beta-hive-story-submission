d2891075def8c4e233fde9a0647f54a13013d4df
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2024-11-23 16:09:41 -0700

    initial commit
    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles/_app.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <p>I am a React app!</p>
+    </div>
+  );
+}
+
+export default App;
