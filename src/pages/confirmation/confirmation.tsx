b06efb18359ccf3e582ae7d53d10ef924db9b292
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2024-12-30 19:59:59 -0700

    initial design and set up of the confirmation

--- src/pages/confirmation/confirmation.tsx
+++ src/pages/confirmation/confirmation.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 export const Confirmation: React.FC = () => {
   return (
-    <div>
-      <p>Confirmation</p>
+    <div className='container-fluid'>
+      <div className='row d-flex justify-content-between align-items-center'>
+        <div className='col'>
+          <h1 className='bd-title pb-2 mt-4'>Confirmation</h1>
+          <p className='text-muted pb-2 mt-2 fs-5'>
+            Thank you! Your story has been submitted and is pending review.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
