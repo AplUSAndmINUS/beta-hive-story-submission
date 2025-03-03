0eb4498054e867b00d6f1493f6beee2e990751ce
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2025-03-03 11:21:06 -0700

    battleHIVE service and APIs

---
+++ src/services/battleHIVE-service.ts
@@ -0,0 +1,20 @@
+import { addStory, getAllStories, updateStory } from './apis/battleHIVE/battleHIVE-api';
+import { storySchema } from './models/battleHIVE.types';
+
+// Function to get all stories
+export const getStories = (): storySchema[] => {
+  return getAllStories();
+};
+
+// Function to update a story
+export const updateStoryById = (
+  id: number,
+  updatedProperties: Partial<storySchema>
+): storySchema | null => {
+  return updateStory(id, updatedProperties);
+};
+
+// Function to add a new story
+export const publishStory = (newStory: storySchema) => {
+  return addStory(newStory);
+};
